7cb03d9a31dd0d133a6525287239554957624928
Alex Beattie <artrisanbranding@me.com>
2022-01-21 13:49:57 -0800

    new

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+
+
     <div>
-      <h1>The Mind Coach</h1>
-      <h2>Alex Beattie & Michele Guzy</h2>
+      <h1>Artisan </h1>
+      <h2>Alex Beattie</h2>
     </div>
   )
 }
